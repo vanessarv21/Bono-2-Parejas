9ca0f97349b223e0496a90b8545f9d1c7e43ad9f
vanessarv21 <109983874+vanessarv21@users.noreply.github.com>
2025-10-23 21:38:32 +0000

    clase nodo

--- bono.py
+++ bono.py
@@ -8,4 +8,17 @@ class Cliente:
     def __str__(self):
         estado = "ACTIVO" if self.activo else "INACTIVO"
         return f"{self.nombre} {self.apellido} - Tel: {self.telefono} - Estado: {estado}"
-    
+
+class Nodo:
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.anterior = None
+        self.siguiente = None
+
+
+class ListaDobleClientes:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamano = 0
+        
