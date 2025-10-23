31b132510ed3773c22ab3e64aeae5c53d451e53b
Maria Angel <166426281+Mariangel309@users.noreply.github.com>
2025-10-23 21:19:55 +0000

    clase cliente lista

--- bono.py
+++ bono.py
@@ -1,2 +1,11 @@
-class cliente: 
+class Cliente:
+    def __init__(self, nombre, apellido, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.activo = True
+        
+    def __str__(self):
+        estado = "ACTIVO" if self.activo else "INACTIVO"
+        return f"{self.nombre} {self.apellido} - Tel: {self.telefono} - Estado: {estado}"
     
